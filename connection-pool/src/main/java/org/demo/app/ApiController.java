ce1956a3305a78fd946e99ce46f528a792342ef4
mac <mac@macdembp>
2019-03-08 10:41:49 +0800

    api 控制器添加header解析

--- connection-pool/src/main/java/org/demo/app/ApiController.java
+++ connection-pool/src/main/java/org/demo/app/ApiController.java
@@ -1,16 +1,28 @@
 package org.demo.app;
 
+import java.util.Enumeration;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(ApiController.class);
 
-	@RequestMapping(value = "/on", method = RequestMethod.GET)
-	public String headerHandler(HttpServletRequest request) {
+	@RequestMapping(value = "/on")
+	public String handler(HttpServletRequest request) {
+		
+		Enumeration<String> headers = request.getHeaderNames();
+		while(headers.hasMoreElements()) {
+			String headerName = headers.nextElement();
+			String headerValue = request.getHeader(headerName);
+			LOG.info("收到headers:{}=>{}", headerName, headerValue);
+		}
 		return StartupListener.getData().toString();
 	}
 
